20ee696b1f010dcf0c244a611658f6329cb250ab
David Garay <garaekz@gmail.com>
2024-03-09 00:23:11 -0800

    Change schemas

--- migrations/20191217202658_init.up.sql
+++ migrations/20191217202658_init.up.sql
@@ -1,7 +1,31 @@
-CREATE TABLE album
+CREATE TABLE users
 (
     id         VARCHAR PRIMARY KEY,
     name       VARCHAR NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
+    email      VARCHAR NOT NULL,
+    password   VARCHAR NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE providers (
+    id VARCHAR PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    image_url VARCHAR(255),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE user_provider_accounts (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    provider_id INT NOT NULL,
+    access_token TEXT NOT NULL,
+    refresh_token TEXT NOT NULL,
+    token_expiry TIMESTAMP NOT NULL,
+    default BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (provider_id) REFERENCES providers(provider_id)
 );
