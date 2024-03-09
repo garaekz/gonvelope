855008a0ca0be724ca192658ee9abc5df6177ff5
David Garay <garaekz@gmail.com>
2024-03-09 00:23:23 -0800

    User id to uuid v7

--- internal/entity/id.go
+++ internal/entity/id.go
@@ -4,5 +4,9 @@ import "github.com/google/uuid"
 
 // GenerateID generates a unique ID that can be used as an identifier for an entity.
 func GenerateID() string {
-	return uuid.New().String()
+	id, err := uuid.NewV7()
+	if err != nil {
+		panic(err)
+	}
+	return id.String()
 }
